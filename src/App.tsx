dd7a01757ee62072811ea51440e5fe7905788b63
sunil-kumar-26 <sunilworld1234ss@gmail.com>
2026-02-23 15:38:16 +0530

    add:dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import styled, { ThemeProvider } from "styled-components";
 import useToggleTheme from "./hooks/useToggleTheme";
 import { darkTheme, lightTheme } from "./theme/theme";
 import Login from "./pages/Login";
+import LayoutPage from "./pages/LayoutPage";
 
 function App() {
   const { isDarkTheme} = useToggleTheme();
@@ -13,7 +14,9 @@ function App() {
       <AppWrapper>
         <ConfigProvider locale={getAntdLanguage()}>
           <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
-            <Login />
+            {/* <Login />
+             */}
+             <LayoutPage/>
           </ThemeProvider>
         </ConfigProvider>
       </AppWrapper>
---
+++ src/utils/simpleIcons.tsx
@@ -0,0 +1,5 @@
+import type { IconType } from "react-icons";
+import { TbLayoutSidebar } from "react-icons/tb";
+import { TbLayoutSidebarFilled } from "react-icons/tb";
+
+export const simpleIcons:IconType[] = [TbLayoutSidebar , TbLayoutSidebarFilled ];
